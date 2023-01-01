f41cd3f4cf9d0c6e29b50a1e2d254caa4591cfe9
Pascal <pascalyuen@gmail.com>
2023-01-02 01:32:28 +0800

    Create append(value) method

--- lib/linked_lists.rb
+++ lib/linked_lists.rb
@@ -1,5 +1,12 @@
 class LinkedList
+  def append(value)
+    Node.new(value)
+  end
 end
 
 class Node
+  def initilize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
 end
